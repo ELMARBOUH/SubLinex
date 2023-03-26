13695fe19cdb35e4e2b93c9cab1299af66c87bf5
KHALID ELMARBOUH <khalid.Elmarbouh.devloper@gmail.com>
2023-03-26 13:46:26 +0000

    Layout.xml : Add  Layout Splash screen + Layout Google authentication

--- app/src/main/java/com/example/sublinex/Splash_Screen.java
+++ app/src/main/java/com/example/sublinex/Splash_Screen.java
@@ -1,16 +1,16 @@
 package com.example.sublinex;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_authentication);
-
+        setContentView(R.layout.activity_splash_screen);
 
     }
 }
